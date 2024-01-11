e69b54ed51610b7f9fb7cac98dfa24bd471d1f90
hadil <hihijazi@gmail.com>
2024-01-10 21:07:35 -0500

    save changes

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(list):
+    if len(list) == 1:
+        return list[0]
+    elif len(list) == 2:
+        return ' and '.join(list)
+    elif len(list) >= 3:
+      list[-1] = 'and ' + list[-1]
+      print(list)
+      return ', '.join(list)
+print(oxford_comma(['kiwi', 'banana', 'hel']))
